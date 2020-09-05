9b78152d646b90d8823d8e878b6ce21d0e8b28ef
aseemsavio <aseemsavio3@gmail.com>
2020-09-05 21:06:23 +0530

    Cors property edited

--- src/main/java/com/aseemsavio/blog/BlogApplication.java
+++ src/main/java/com/aseemsavio/blog/BlogApplication.java
@@ -9,20 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				//registry.addMapping("/signIn").allowedOrigins("http://localhost:4200");
-				registry.addMapping("/**");
-			}
-		};
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 
 
 }
